c551393e22bf14853f37c564bbf872df913416ad
mwiehe <m.wiehe@cern.ch>
2022-04-08 22:54:29 +0200

    added prefix G

--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 import matplotlib.pyplot as plt
 
-prefix = {"m": 1e3, "u": 1e6, "n": 1e9, "p": 1e12, "k": 1e-3, "M": 1e-6, "": 1}
+prefix = {
+    "m": 1e3,
+    "u": 1e6,
+    "n": 1e9,
+    "p": 1e12,
+    "k": 1e-3,
+    "M": 1e-6,
+    "G": 1e-9,
+    "": 1,
+}
 
 
 def show_plots():
